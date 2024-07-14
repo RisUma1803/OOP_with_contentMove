23b9074098a4d46def3e40e5af097b5edf17293f
RisUma1803 <umidjonergashev1805@gmail.com>
2024-07-14 11:35:21 +0500

    finishing the assignment

---
+++ script.js
@@ -0,0 +1,53 @@
+// // OOP Object Oriented Programming
+// // FOP Functional Oriented Programming
+
+
+// // // console.log(new Date());
+// // // function date() {
+
+// // }
+// let a = 2
+// class Human {
+//     constructor({ name, age, surname, gender}) {
+//         if (this.constructor === Human) {
+//             console.log("San Abstraction class yarata olmaysan");
+//         }
+//         this.name = name
+//         this.age = age
+//         this.surname = surname
+//         this.gender = gender
+  
+//     }
+//     getInfo() {
+//         return `Your name ${this.name}. Your age is ${this.age}, Your surname is ${this.surname}, Gender ${this.gender}`
+//     }
+// }
+// const alisher = new Human({
+//     name: "Alisher",
+//     age: 25,
+//     surname: "Rasulov",
+//     gender: 'Male'
+// })
+// const Javohir = new Human({
+//     name: "Javohir",
+//     age: 18,
+//     surname: 'Mahmudov',
+//     gender: 'Male'
+// })
+// console.log(alisher.getInfo());
+// class Woman extends Human {
+//     constructor(props) {
+//         super(props)
+//     }
+//     method() {
+//         return gender
+//     }
+
+// }
+// const Ziyoda = new Woman({
+//     name: "Ziyoda",
+//     age: 21,
+//     surname: "SSS",
+//     gender: 'female'
+// })
+// console.log(Ziyoda.getInfo());
---
+++ type.js
@@ -0,0 +1,50 @@
+const title = document.querySelector(".header__content h1"),
+  text = title.innerHTML;
+
+class Type {
+  constructor({ el, interval, delay }) {
+    this.el = document.querySelector(el);
+    this.interval = interval || 500;
+    this.delay = delay ?? 500;
+    this.text = this.el.innerHTML.trim();
+    this.el.innerHTML = "";
+    setTimeout(() => {
+      this.write();
+    }, this.delay);
+  }
+  write(i = 0) {
+    this.el.innerHTML += this.text[i];
+    i++;
+    if (i < this.text.length) {
+      setTimeout(() => {
+        this.write(i);
+      }, this.interval);
+    }
+  }
+}
+
+new Type({
+  el: ".header__content h1",
+  interval: 300,
+  delay: 1000,
+});
+new Type({
+  el: ".info__scroll-desc",
+  interval: 100,
+  delay: 500,
+});
+
+// mouseover
+
+const content = document.querySelector(".header__content");
+const nav = document.querySelector(".header__nav");
+
+content.addEventListener('mouseover', () => {
+    content.style.marginTop = `${moveRandomly(0, window.innerHeight - content.clientHeight - nav.clientHeight)}px`;
+    content.style.marginLeft = `${moveRandomly(0, window.innerWidth - content.clientWidth)}px`;
+});
+
+function moveRandomly(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
